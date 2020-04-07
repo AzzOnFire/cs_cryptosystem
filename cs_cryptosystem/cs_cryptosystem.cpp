c7c4dc44fb3192834a270ec55d692613f4398df3
AzzOnFire <43025779+AzzOnFire@users.noreply.github.com>
2020-04-07 20:17:07 +0300

    First commit

---
+++ cs_cryptosystem/cs_cryptosystem.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+typedef unsigned char byte;
+
+/**
+ * Функция g, осуществляющая генерацию производного ключа на основе ключа зашифрования и вектора инициализации
+ *
+ * @param key           Ключ зашифрования
+ * @param init_vector   Вектор инициализации
+ * @return derived_key  Производный ключ, сгенерированный по общему ключу и вектору инициализации
+ */
+template <size_t n>
+byte * g_function(const byte* key, const byte* init_vector) {
+    
+}
+
+template <size_t n>
+byte * init_vector() {
+
+}
+
+template <size_t n>
+void cs_encrypt(const byte* key, const byte* plain) {
+    byte* init_vector;
+    byte* derived_key = g_function<n>(key, init_vector);
+
+}
+
+template <size_t n>
+void cs_algorithm(const byte* derived_key) {
+
+}
+
+int main() {
+    std::cout << "Hello World!\n";
+}
