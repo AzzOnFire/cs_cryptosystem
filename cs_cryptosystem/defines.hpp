369c765fe8f1f255e812d76b6f6290e4a4af59b5
Сергей Щербаков <scherbakov.sergey24@gmail.com>
2020-05-04 14:43:49 +0300

    в defines.hpp описаны порождающие многочлены для полей. если где-то надо используйте только их

--- cs_cryptosystem/defines.hpp
+++ cs_cryptosystem/defines.hpp
@@ -6,3 +6,11 @@ typedef unsigned char byte;
 #define bits_to_word(bits)  bits/16
 #define bits_to_dword(bits) bits/32
 #define bits_to_qword(bits) bits/64
+
+
+// Generative polynoms:
+// GF(2^8)		x^8 + x^4 + x^3 + x^2 + 1
+// GF(2^64)		x^64 + x^4 + x^3 + x + 1
+// GF(2^128)	x^128 + x^7 + x^2 + x + 1
+// GF(2^256)	x^256 + x^10 + x^5 + x^2 + 1
+// GF(2^512)	x^512 + x^8 + x^5 + x^2 + 1
